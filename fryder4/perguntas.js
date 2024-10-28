f32d0b54dd0066785fae421a65be633b270c0fe1
emanuellyfryder <145142177+emanuellyfryder@users.noreply.github.com>
2024-10-28 12:15:16 -0300

    Add files via upload

---
+++ fryder4/perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'eu',
+    'quem sou eu?',
+    'eu sou a emanuelly  e tenho 16 anos '
+)
+
+criaCartao(
+    'sobre familia',
+    'minha familia é boa mais também tem seus defeitos',
+    
+)
+
+criaCartao(
+    'minha casa ',
+    'onde moro?',
+    'moro em pinhão-PR, no bairro vila Caldas,longitude-25.707320, latitude -51.645064'
+)
+
+criaCartao(
+    'minha escola',
+    'onde estudo?',
+    'colegio estadual Mario Evaldo Morski '
+)
+
+criaCartao(
+    'amigos e turma  ',
+    'miinha turma como é, meus amigos?',
+    'muitos alunos legais e inteligentes e outos nem tanto  '
+)
+
+criaCartao(
+    'faculdade',
+    'qual faculdade pensa em fazer?',
+    'quero fazer  direito '
+)
+
+criaCartao(
+    'meus sonhos  ',
+    'minha cidade como é pra mim?',
+    'pra mim é boa '
+)
+
+criaCartao(
+    'oitava   pergunta ',
+    'meu sonho?',
+    'fazer minha faculdade e ter uma casa pra mim'
+)
+
+
+
+
+
+    
+
